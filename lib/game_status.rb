b50140ccd9ea4a7569c7ed48dcc985fa164745a0
kello22e <kello22e@mtholyoke.edu>
2018-06-02 21:45:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,7 +56,7 @@ end
 def winner(board)
   if won?(board)
     win_combination = won?(board)
-    if board[0] == "X"
+    if board[win_combination[0]] == "X"
       return "X"
     else
       return "O"
