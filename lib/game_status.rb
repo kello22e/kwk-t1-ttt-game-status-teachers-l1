45532d2654a6e2cbc701a023f1e2df9eafdc07fe
kello22e <kello22e@mtholyoke.edu>
2018-06-02 21:33:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def win?(board)
     position_1 = board[win_combination[0]] 
     position_2 = board[win_combination[1]] 
     position_3 = board[win_combination[2]] 
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") 
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
       return win_combination 
     else
      return false
