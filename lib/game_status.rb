3dc9b820c91503ab757612dff8b62dfa9b8fde8f
kello22e <kello22e@mtholyoke.edu>
2018-06-02 21:33:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,36 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def full?(board)
+  index = 0
+  while index < board.length
+    if board[index] == " " then
+      return false
+    end
+  end
+  return true
+end
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]]
+
+def win?(board) 
+  WIN_COMBINATIONS.each do |win_combination|
+    position_1 = board[win_combination[0]] 
+    position_2 = board[win_combination[1]] 
+    position_3 = board[win_combination[2]] 
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") 
+      return win_combination 
+    else
+     return false
+    end
+  end
+end
